8de5fbba4ad6a75bb4a83f55cd140d39fe8e5ee8
Paxton Proctor <61135201+paxtonproctor@users.noreply.github.com>
2021-03-11 12:30:45 -0600

    Add files via upload

---
+++ Program 2/Proctor_prog2Orig.cpp
@@ -0,0 +1,127 @@
+/************************************************************************
+*                                                                       *
+* Name: Paxton Proctor                                                  *
+*                                                                       *
+* Course: CMPS-2433-201 , spring 21, Springfellow                       *
+*                                                                       *
+* Purpose:  Reinforce the concepts of functions;                        *
+*also uses while !eof loop pattern and iomanip.h in C++                 *
+************************************************************************/
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+// function Prototypes
+void openFiles(ifstream& infile, ofstream& outfile);
+void headings(ifstream& infile, ofstream& outfile, double& x);
+void evalFunctions(ifstream& infile, ofstream& outfile, double& x);
+void evalInversefun(ifstream& infile, ofstream& outfile, double& x);
+void evalFunomp(ifstream& infile, ofstream& outfile, double& x);
+void evalFuncompinverse(ifstream& infile, ofstream& outfile, double& x);
+
+//Main
+int main() {
+
+	// variable X for infile
+	double X;
+
+	// Declare input file
+	ifstream infile;
+	ofstream outfile;
+
+	// calls the heading and input file
+	openFiles(infile, outfile);
+	headings(infile, outfile, X);
+
+	// reads to the end of file
+	while (!infile.eof()) {
+		infile >> X;
+		outfile << setw(5) << X;
+		// calls functions and inverses of them
+		evalFunctions(infile, outfile, X);
+		evalInversefun(infile, outfile, X);
+		evalFunomp(infile, outfile, X);
+		evalFuncompinverse(infile, outfile, X);
+		outfile << '\n';
+	}
+	
+	// Closes infile and outfile
+	infile.close();
+	outfile.close();
+	return 0;
+}
+/************************************************************************
+* Purpose: opens input file and ask user for the file                   *
+* Recieves: infile and outfile reference                                *
+* Returns: nothing                                                      *
+************************************************************************/
+void openFiles(ifstream& infile, ofstream& outfile) {
+	char infileName[40];
+	char outfileName[40];
+
+	// ask user for filename
+	cout << "Enter the input file name: ";
+	cin >> infileName;
+	infile.open(infileName);
+
+	// ask user for outfile name
+	cout << "Enter the output file name: ";
+	cin >> outfileName;
+	outfile.open(outfileName);
+}
+/************************************************************************
+* Purpose: creates and organizes the heading and prints the format      *
+* Recieves: infile,outfile, and x reference                             *
+* Returns: nothing                                                      *
+************************************************************************/
+void headings(ifstream& infile, ofstream& outfile, double& x) {
+	outfile << fixed << setprecision(2) << "Student: Paxton Proctor \n";
+	outfile << "Program 2: Function Compositions\n";
+	outfile << "f(x) = 3x+5  g(x) =  3x/5 + 1  f-1(y) = (y-5)/3"
+		<< "  g-1(y) = (5y-5)/3\n\n";
+	outfile << setw(3) << "x" << setw(7) << "f(x)" << setw(9) << "g(x)" <<
+		setw(9) << "fInv(y)" << setw(11) << "gInv(y)" << setw(10)
+		<< "f(g(x))" << setw(11) << "g(f(x))" << setw(12) << "fInv(f(x))" <<
+		setw(13) << " gInv(g(x))\n";
+	outfile << "-------------------------------------------------" <<
+		"--------------------------------------\n";
+}
+/************************************************************************
+* Purpose: Prints function answer of f(x) and g(x)                      *
+* Recieves: infile,outfile, and x reference                             *
+* Returns: nothing                                                      *
+************************************************************************/
+void evalFunctions(ifstream& infile, ofstream& outfile, double& x) {
+	outfile << setw(8) << (3 * x) + 5 << setw(8) << ((3 * x) / 5) + 1;
+}
+/************************************************************************
+* Purpose: Prints inverse function answer of f(x) and g(x)              *
+* Recieves: infile,outfile, and x reference                             *
+* Returns: nothing                                                      *
+************************************************************************/
+void evalInversefun(ifstream& infile, ofstream& outfile, double& x) {
+	outfile << setw(8) << (x - 5) / 3 << setw(8) << ((5 * x) - 5) / 3;
+}
+/************************************************************************
+* Purpose: Prints composition of function answer of f(g(x)) and g(f(x)) *
+* Recieves: infile,outfile, and x reference                             *
+* Returns: nothing                                                      *
+************************************************************************/
+void evalFunomp(ifstream& infile, ofstream& outfile, double& x) {
+	outfile << setw(9) << 3 * ((3 * x) / 5 + 1) + 5 
+		<< setw(9) << (3 * ((3 * x) + 5)) / 5 + 1;
+}
+/************************************************************************
+* Purpose: Prints composition of inverse function                       *
+* answer of f(f(x)) and g(g(x))                                         *
+* Recieves: infile,outfile, and x reference                             *
+* Returns: nothing                                                      *
+************************************************************************/
+void evalFuncompinverse(ifstream& infile, ofstream& outfile, double& x) {
+	outfile << setw(10) << (((3 * x) + 5) - 5) / 3 
+		<< setw(10) << ((5 * (((3 * x) / 5) + 1)) - 5) / 3;
+}
